345534b9b4a75556dcaa0e0da59c1e671934c24b
Ryan Manchester <ryanm17@gmail.com>
2018-05-08 03:13:24 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2) do
+ActiveRecord::Schema.define(version: 3) do
 
   create_table "students", force: :cascade do |t|
     t.string "name"
-    t.string "birthdate"
+    t.datetime "birthdate"
     t.integer "grade"
   end
 
